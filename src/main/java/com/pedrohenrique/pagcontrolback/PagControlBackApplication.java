fee27cefaf9594d66b4baa38c4e66c3cda5f6787
Pedro Henrique <ph6704938@gmail.com>
2026-01-30 19:50:29 -0300

    initial commit

---
+++ src/main/java/com/pedrohenrique/pagcontrolback/PagControlBackApplication.java
@@ -0,0 +1,13 @@
+package com.pedrohenrique.pagcontrolback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagControlBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PagControlBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pedrohenrique/pagcontrolback/PagControlBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedrohenrique.pagcontrolback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagControlBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
