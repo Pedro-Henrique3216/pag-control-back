c41257628f6065e0b39d965134aa2b3d03b0c92a
Pedro Henrique <ph6704938@gmail.com>
2026-01-31 18:46:25 -0300

    feat: add UserDomainException class for handling user-related errors

---
+++ src/main/java/com/pedrohenrique/pagcontrolback/exceptions/UserDomainException.java
@@ -0,0 +1,7 @@
+package com.pedrohenrique.pagcontrolback.exceptions;
+
+public class UserDomainException extends RuntimeException {
+    public UserDomainException(String message) {
+        super(message);
+    }
+}
