815bc6b1ce82bf0b85ca4ee1f0a066bc674640e3
Pedro Henrique <ph6704938@gmail.com>
2026-02-18 12:16:27 -0300

    feat(auth): generate jwt token

---
+++ src/main/java/com/pedrohenrique/pagcontrolback/config/security/TokenService.java
@@ -0,0 +1,37 @@
+package com.pedrohenrique.pagcontrolback.config.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.pedrohenrique.pagcontrolback.model.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    public String generateToken(User user) {
+        try {
+            return JWT.create()
+                    .withSubject(user.getId().toString())
+                    .withClaim("email", user.getEmail())
+                    .withIssuer("pagcontrol")
+                    .withExpiresAt(generateExpirationDate())
+                    .sign(Algorithm.HMAC256(secret));
+        } catch (JWTCreationException exception) {
+            throw new RuntimeException("Error generating token", exception);
+        }
+
+    }
+
+    private Instant generateExpirationDate(){
+        return LocalDateTime.now().plusDays(5).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
