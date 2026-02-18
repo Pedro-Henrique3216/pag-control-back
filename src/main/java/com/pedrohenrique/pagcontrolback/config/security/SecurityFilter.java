a719df15f52f147a5bdd298206f768c9aa2fea13
Pedro Henrique <ph6704938@gmail.com>
2026-02-18 12:51:49 -0300

    refactor(auth): clean up imports in SecurityFilter

--- src/main/java/com/pedrohenrique/pagcontrolback/config/security/SecurityFilter.java
+++ src/main/java/com/pedrohenrique/pagcontrolback/config/security/SecurityFilter.java
@@ -1,18 +1,14 @@
 package com.pedrohenrique.pagcontrolback.config.security;
 
 import com.pedrohenrique.pagcontrolback.repositories.UserRepository;
-import jakarta.persistence.Column;
 import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import java.io.IOException;
 import java.util.UUID;
 
 @Component
