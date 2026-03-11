d0eafe1d43b09ac2964f04980f62537696f1a670
Pedro Henrique <ph6704938@gmail.com>
2026-03-11 19:28:54 -0300

    feat(category): update DTOs to use consistent JSON property naming for category type

--- src/main/java/com/pedrohenrique/pagcontrolback/dtos/request/CategoryRequestDto.java
+++ src/main/java/com/pedrohenrique/pagcontrolback/dtos/request/CategoryRequestDto.java
@@ -1,5 +1,6 @@
 package com.pedrohenrique.pagcontrolback.dtos.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.pedrohenrique.pagcontrolback.model.CategoryType;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -8,6 +9,7 @@ public record CategoryRequestDto(
         @NotBlank(message = "Name is required.")
         String name,
         @NotNull(message = "Category type is required.")
+        @JsonProperty(value = "category_type")
         CategoryType categoryType
 ) {
 }
--- src/main/java/com/pedrohenrique/pagcontrolback/dtos/response/CategoryResponseDto.java
+++ src/main/java/com/pedrohenrique/pagcontrolback/dtos/response/CategoryResponseDto.java
@@ -1,5 +1,6 @@
 package com.pedrohenrique.pagcontrolback.dtos.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.pedrohenrique.pagcontrolback.model.CategoryType;
 
 import java.util.UUID;
@@ -7,6 +8,7 @@ import java.util.UUID;
 public record CategoryResponseDto(
         UUID id,
         String name,
-        CategoryType type
+        @JsonProperty(value = "category_type")
+        CategoryType categoryType
 ) {
 }
