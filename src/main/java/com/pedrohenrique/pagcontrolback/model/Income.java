c01a0b0aac50bd013f46f6e081dbc90466122b04
Pedro Henrique <ph6704938@gmail.com>
2026-04-03 23:32:58 -0300

    feat(income): add Income entity with validation and category assignment

---
+++ src/main/java/com/pedrohenrique/pagcontrolback/model/Income.java
@@ -0,0 +1,94 @@
+package com.pedrohenrique.pagcontrolback.model;
+
+import com.pedrohenrique.pagcontrolback.exceptions.CategoryTypeInvalidException;
+import com.pedrohenrique.pagcontrolback.exceptions.InvalidAmountException;
+import com.pedrohenrique.pagcontrolback.exceptions.UserRequiredException;
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@Table(name = "incomes")
+public class Income {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    @Column(precision = 19, scale = 2, nullable = false)
+    private BigDecimal amount;
+    @Column(length = 100, nullable = false)
+    private String description;
+    private LocalDate date;
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    public Income() {}
+
+    public Income(BigDecimal amount, String description, LocalDate date, User user) {
+        if (amount == null || amount.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new InvalidAmountException("Amount must be greater than zero");
+        }
+        this.amount = amount;
+        this.description = description;
+        this.date = date;
+        setUser(user);
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setUser(User user) {
+        try {
+            this.user = Objects.requireNonNull(user);
+        } catch (NullPointerException e) {
+            throw new UserRequiredException("User cannot be null");
+        }
+    }
+
+    public void assignCategory(Category category) {
+        if(category.getCategoryType() != CategoryType.INCOME){
+            throw new CategoryTypeInvalidException("Category type must be INCOME");
+        }
+        this.category = category;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Income income = (Income) o;
+        return Objects.equals(id, income.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
