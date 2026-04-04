2417f5cfabc167a778c53ec1648c590d45a83a92
Pedro Henrique <ph6704938@gmail.com>
2026-04-04 15:55:30 -0300

    test(categoryController): refactor tests to use nested classes for category creation and get

--- src/test/java/com/pedrohenrique/pagcontrolback/controllers/CategoryControllerTest.java
+++ src/test/java/com/pedrohenrique/pagcontrolback/controllers/CategoryControllerTest.java
@@ -8,6 +8,7 @@ import com.pedrohenrique.pagcontrolback.repositories.UserRepository;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -52,7 +53,6 @@ class CategoryControllerTest {
                 port
         );
 
-
         token = authTestFactory.loginAndGetToken(
                 port,
                 "teste@gmail.com",
@@ -60,126 +60,144 @@ class CategoryControllerTest {
         );
     }
 
-    @Test
-    void shouldCreateCategorySuccessfully() {
 
-        CategoryRequestDto body = new CategoryRequestDto(
-                "food",
-                CategoryType.EXPENSE
-        );
+    @Nested
+    class CreateCategory {
 
-        given()
-                .contentType(ContentType.JSON)
-                .header("Authorization", "Bearer " + token)
-                .body(body)
-                .when()
-                .post()
-                .then()
-                .statusCode(201)
-                .body("id", notNullValue())
-                .body("name", equalTo("food"))
-                .body("category_type", equalTo("EXPENSE"));
+        @Nested
+        class Success {
+
+            @Test
+            void shouldCreateCategorySuccessfully() {
+
+                CategoryRequestDto body = new CategoryRequestDto(
+                        "food",
+                        CategoryType.EXPENSE
+                );
+
+                given()
+                        .contentType(ContentType.JSON)
+                        .header("Authorization", "Bearer " + token)
+                        .body(body)
+                        .when()
+                        .post()
+                        .then()
+                        .statusCode(201)
+                        .body("id", notNullValue())
+                        .body("name", equalTo("food"))
+                        .body("category_type", equalTo("EXPENSE"));
+            }
+        }
+
+        @Nested
+        class Errors {
+
+            @Test
+            void shouldReturn400WhenNameIsEmpty() {
+
+                CategoryRequestDto body = new CategoryRequestDto(
+                        "",
+                        CategoryType.EXPENSE
+                );
+
+                given()
+                        .contentType(ContentType.JSON)
+                        .header("Authorization", "Bearer " + token)
+                        .body(body)
+                        .when()
+                        .post()
+                        .then()
+                        .statusCode(400);
+            }
+
+            @Test
+            void shouldReturn400WhenCategoryTypeIsMissing() {
+
+                CategoryRequestDto body = new CategoryRequestDto(
+                        "food",
+                        null
+                );
+
+                given()
+                        .contentType(ContentType.JSON)
+                        .header("Authorization", "Bearer " + token)
+                        .body(body)
+                        .when()
+                        .post()
+                        .then()
+                        .statusCode(400);
+            }
+
+            @Test
+            void shouldReturn409WhenCategoryAlreadyExists() {
+
+                CategoryRequestDto body = new CategoryRequestDto(
+                        "food",
+                        CategoryType.EXPENSE
+                );
+
+                given()
+                        .contentType(ContentType.JSON)
+                        .header("Authorization", "Bearer " + token)
+                        .body(body)
+                        .when()
+                        .post()
+                        .then()
+                        .statusCode(201);
+
+                given()
+                        .contentType(ContentType.JSON)
+                        .header("Authorization", "Bearer " + token)
+                        .body(body)
+                        .when()
+                        .post()
+                        .then()
+                        .statusCode(409);
+            }
+
+            @Test
+            void shouldReturn403WhenUserNotAuthenticated() {
+
+                CategoryRequestDto body = new CategoryRequestDto(
+                        "food",
+                        CategoryType.EXPENSE
+                );
+
+                given()
+                        .contentType(ContentType.JSON)
+                        .body(body)
+                        .when()
+                        .post()
+                        .then()
+                        .statusCode(403);
+            }
+        }
     }
 
-    @Test
-    void shouldReturn400WhenNameIsEmpty() {
 
-        CategoryRequestDto body = new CategoryRequestDto(
-                "",
-                CategoryType.EXPENSE
-        );
+    @Nested
+    class GetCategories {
 
-        given()
-                .contentType(ContentType.JSON)
-                .header("Authorization", "Bearer " + token)
-                .body(body)
-                .when()
-                .post()
-                .then()
-                .statusCode(400);
+        @Nested
+        class Success {
+
+            @Test
+            void shouldReturn200WhenFindAllCategoriesByUser() {
+
+                categoryFactory.createCategoryExpense(port, token);
+
+                given()
+                        .contentType(ContentType.JSON)
+                        .header("Authorization", "Bearer " + token)
+                        .when()
+                        .get()
+                        .then()
+                        .statusCode(200)
+                        .body("size()", equalTo(1))
+                        .body("[0].id", notNullValue())
+                        .body("[0].name", equalTo("teste"))
+                        .body("[0].category_type", equalTo("EXPENSE"));
+            }
+        }
     }
-
-    @Test
-    void shouldReturn400WhenCategoryTypeIsMissing() {
-
-        CategoryRequestDto body = new CategoryRequestDto(
-                "food",
-                null
-        );
-
-        given()
-                .contentType(ContentType.JSON)
-                .header("Authorization", "Bearer " + token)
-                .body(body)
-                .when()
-                .post()
-                .then()
-                .statusCode(400);
-    }
-
-    @Test
-    void shouldReturn409WhenCategoryAlreadyExists() {
-
-        CategoryRequestDto body = new CategoryRequestDto(
-                "food",
-                CategoryType.EXPENSE
-        );
-
-
-        given()
-                .contentType(ContentType.JSON)
-                .header("Authorization", "Bearer " + token)
-                .body(body)
-                .when()
-                .post()
-                .then()
-                .statusCode(201);
-
-        given()
-                .contentType(ContentType.JSON)
-                .header("Authorization", "Bearer " + token)
-                .body(body)
-                .when()
-                .post()
-                .then()
-                .statusCode(409);
-    }
-
-    @Test
-    void shouldReturn401WhenUserNotAuthenticated() {
-
-        CategoryRequestDto body = new CategoryRequestDto(
-                "food",
-                CategoryType.EXPENSE
-        );
-
-        given()
-                .contentType(ContentType.JSON)
-                .body(body)
-                .when()
-                .post()
-                .then()
-                .statusCode(403);
-    }
-
-    @Test
-    void shouldReturn200WhenFindAllCategoriesByUser() {
-        categoryFactory.createCategoryExpense(
-                port,
-                token
-        );
-
-        given()
-            .contentType(ContentType.JSON)
-                .header("Authorization", "Bearer " + token)
-                .when()
-                .get()
-                .then()
-                .statusCode(200)
-                .body("size()", equalTo(1))
-                .body("[0].id", notNullValue())
-                .body("[0].name", equalTo("teste"))
-                .body("[0].category_type", equalTo("EXPENSE"));
-    }
-
 }
