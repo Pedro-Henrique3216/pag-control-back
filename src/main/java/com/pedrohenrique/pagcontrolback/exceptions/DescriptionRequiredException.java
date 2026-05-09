5c06a6b0e3d57c6a7dc917ba3c95fae29ce50e4f
Pedro Henrique <ph6704938@gmail.com>
2026-05-08 21:10:33 -0300

    feat(exceptions): add CategoryRequiredException and DescriptionRequiredException classes

---
+++ src/main/java/com/pedrohenrique/pagcontrolback/exceptions/DescriptionRequiredException.java
@@ -0,0 +1,7 @@
+package com.pedrohenrique.pagcontrolback.exceptions;
+
+public class DescriptionRequiredException extends RuntimeException {
+    public DescriptionRequiredException(String message) {
+        super(message);
+    }
+}
