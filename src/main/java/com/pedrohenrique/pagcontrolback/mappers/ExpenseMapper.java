d5088914bc4413fa511780c4656f8d648977fe39
Pedro Henrique <ph6704938@gmail.com>
2026-06-01 11:06:53 -0300

    feat(ExpenseMapper): update total amount calculation to use Money value object

--- src/main/java/com/pedrohenrique/pagcontrolback/mappers/ExpenseMapper.java
+++ src/main/java/com/pedrohenrique/pagcontrolback/mappers/ExpenseMapper.java
@@ -1,5 +1,6 @@
 package com.pedrohenrique.pagcontrolback.mappers;
 
+import com.pedrohenrique.pagcontrolback.ValueObjects.Money;
 import com.pedrohenrique.pagcontrolback.dtos.response.ExpenseResponseDto;
 import com.pedrohenrique.pagcontrolback.model.Expense;
 import com.pedrohenrique.pagcontrolback.model.Installment;
@@ -16,10 +17,7 @@ public class ExpenseMapper {
                 expense.getPaymentType(),
                 expense.getSupplier() == null ? null : expense.getSupplier().getId(),
                 expense.getExpenseDate(),
-                expense.getInstallments()
-                        .stream()
-                        .map(Installment::getAmount)
-                        .reduce(BigDecimal.ZERO, BigDecimal::add),
+                expense.getTotalAmount().value(),
                 expense.getCategory() == null ? null : expense.getCategory().getId(),
                 expense.getInstallments()
                         .stream()
