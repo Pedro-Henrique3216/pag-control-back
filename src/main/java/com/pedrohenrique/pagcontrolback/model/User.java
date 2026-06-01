d10834d65d46fdd53d45c9d4b7b4f675315646bd
Pedro Henrique <ph6704938@gmail.com>
2026-06-01 10:20:55 -0300

    feat(User): add createdAt and updatedAt fields with validation

--- src/main/java/com/pedrohenrique/pagcontrolback/model/User.java
+++ src/main/java/com/pedrohenrique/pagcontrolback/model/User.java
@@ -3,6 +3,7 @@ package com.pedrohenrique.pagcontrolback.model;
 import com.pedrohenrique.pagcontrolback.exceptions.UserDomainException;
 import jakarta.persistence.*;
 
+import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
@@ -32,6 +33,10 @@ public class User {
     private Set<Category> categories =  new HashSet<>();
     @OneToMany(mappedBy = "user")
     private Set<Supplier> suppliers = new HashSet<>();
+    @Column(nullable = false, name = "created_at")
+    private LocalDateTime  createdAt;
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 
     public User(
             String name,
@@ -41,13 +46,14 @@ public class User {
             String phone,
             PersonType personType
     ) {
-        validatePersonType(personType, fantasyName);
+        validateUser(name, fantasyName, email, password, phone, personType);
         this.name = name;
         this.fantasyName = fantasyName;
-        this.email = email;
+        this.email = email.toLowerCase().trim();
         this.password = password;
         this.phone = normalizePhone(phone);
         this.personType = personType;
+        this.createdAt = LocalDateTime.now();
     }
 
     public User() {
@@ -65,6 +71,40 @@ public class User {
         if (personType == PersonType.PJ && (fantasyName == null || fantasyName.isBlank()))
             throw new UserDomainException("Fantasy name cannot be null or blank for companies (PJ)"); {
         }
+    }
+
+    private void validateUser(String name, String fantasyName, String email, String password, String phone, PersonType personType) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new UserDomainException("Name cannot be null or empty");
+        }
+
+        if (email == null || email.trim().isEmpty()) {
+            throw new UserDomainException("Email cannot be null or empty");
+        }
+
+        if (!email.matches("^[A-Za-z0-9+_.-]+@(.+)$")) {
+            throw new UserDomainException("Email format is invalid");
+        }
+
+        if (password == null || password.trim().isEmpty()) {
+            throw new UserDomainException("Password cannot be null or empty");
+        }
+
+
+        if (password.length() < 8) {
+            throw new UserDomainException("Password must be at least 8 characters");
+        }
+
+        if (phone == null || phone.trim().isEmpty()) {
+            throw new UserDomainException("Phone cannot be null or empty");
+        }
+
+        phone = normalizePhone(phone);
+        if (phone.length() != 11) {
+            throw new UserDomainException("Phone must be 11 digits");
+        }
+
+        validatePersonType(personType, fantasyName);
     }
 
     private String normalizePhone(String phone) {
@@ -105,6 +145,14 @@ public class User {
 
     public Set<Category> getCategories() {
         return categories;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
     }
 
     public void setPassword(String password) {
