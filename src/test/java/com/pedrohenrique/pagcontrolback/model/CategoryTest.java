69274a15deb42121f4e0ce034fac78ad3eb82e42
Pedro Henrique <ph6704938@gmail.com>
2026-06-05 09:35:21 -0300

    test(CategoryTest): restructure tests into nested classes and enhance validation for category creation

--- src/test/java/com/pedrohenrique/pagcontrolback/model/CategoryTest.java
+++ src/test/java/com/pedrohenrique/pagcontrolback/model/CategoryTest.java
@@ -2,54 +2,126 @@ package com.pedrohenrique.pagcontrolback.model;
 
 import com.pedrohenrique.pagcontrolback.exceptions.CategoryNameInvalidException;
 import com.pedrohenrique.pagcontrolback.exceptions.CategoryTypeInvalidException;
+import com.pedrohenrique.pagcontrolback.exceptions.UserRequiredException;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 class CategoryTest {
 
-    @Test
-    void shouldCreateCategorySuccessfully() {
-        Category category = new Category("Food", TransactionType.EXPENSE, new User());
+    @Nested
+    class CreationTests {
 
-        assertEquals("food", category.getName());
-        assertEquals(TransactionType.EXPENSE, category.getCategoryType());
+        @Test
+        void shouldCreateCategorySuccessfully() {
+            Category category = new Category(
+                    "Food",
+                    TransactionType.EXPENSE,
+                    new User()
+            );
+
+            assertEquals("food", category.getName());
+            assertEquals(TransactionType.EXPENSE, category.getCategoryType());
+            assertNotNull(category.getCreatedAt());
+            assertTrue(category.getActive());
+        }
+
+        @Test
+        void shouldCreateIncomeCategorySuccessfully() {
+            Category category = new Category(
+                    "Salary",
+                    TransactionType.INCOME,
+                    new User()
+            );
+
+            assertEquals(TransactionType.INCOME, category.getCategoryType());
+        }
     }
 
-    @Test
-    void shouldNormalizeName() {
-        Category category = new Category("   Food   ", TransactionType.EXPENSE, new User());
+    @Nested
+    class NameValidationTests {
 
-        assertEquals("food", category.getName());
+        @Test
+        void shouldThrowExceptionWhenNameIsNull() {
+            assertThrows(
+                    CategoryNameInvalidException.class,
+                    () -> new Category(
+                            null,
+                            TransactionType.EXPENSE,
+                            new User()
+                    )
+            );
+        }
+
+        @Test
+        void shouldThrowExceptionWhenNameIsBlank() {
+            assertThrows(
+                    CategoryNameInvalidException.class,
+                    () -> new Category(
+                            "   ",
+                            TransactionType.EXPENSE,
+                            new User()
+                    )
+            );
+        }
     }
 
-    @Test
-    void shouldThrowExceptionWhenNameIsNull() {
-        assertThrows(CategoryNameInvalidException.class, () ->
-            new Category(null, TransactionType.EXPENSE, new User())
-        );
+    @Nested
+    class CategoryTypeValidationTests {
+
+        @Test
+        void shouldThrowExceptionWhenCategoryTypeIsNull() {
+            assertThrows(
+                    CategoryTypeInvalidException.class,
+                    () -> new Category(
+                            "Food",
+                            null,
+                            new User()
+                    )
+            );
+        }
     }
 
-    @Test
-    void shouldThrowExceptionWhenNameIsEmpty() {
-        assertThrows(CategoryNameInvalidException.class, () ->
-            new Category("   ", TransactionType.EXPENSE, new User())
-        );
+    @Nested
+    class UserValidationTests {
+
+        @Test
+        void shouldThrowExceptionWhenUserIsNull() {
+            assertThrows(
+                    UserRequiredException.class,
+                    () -> new Category(
+                            "Food",
+                            TransactionType.EXPENSE,
+                            null
+                    )
+            );
+        }
     }
 
-    @Test
-    void shouldReturnCorrectCategoryType() {
-        Category category = new Category("salary", TransactionType.INCOME, new User());
+    @Nested
+    class EqualityTests {
 
-        assertEquals(TransactionType.INCOME, category.getCategoryType());
+        @Test
+        void shouldBeEqualWhenComparingSameInstance() {
+            Category category = new Category(
+                    "Food",
+                    TransactionType.EXPENSE,
+                    new User()
+            );
+
+            assertEquals(category, category);
+        }
+
+        @Test
+        void shouldNotBeEqualToNull() {
+            Category category = new Category(
+                    "Food",
+                    TransactionType.EXPENSE,
+                    new User()
+            );
+
+            assertNotEquals(null, category);
+        }
     }
-
-    @Test
-    void shouldThrowExceptionWhenCategoryTypeIsNull() {
-        assertThrows(CategoryTypeInvalidException.class, () ->
-            new Category("Food", null, new User())
-        );
-    }
-
 }
