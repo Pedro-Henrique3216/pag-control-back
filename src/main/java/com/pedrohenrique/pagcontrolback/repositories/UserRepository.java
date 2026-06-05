ce5ea5a543e3a4bf57c8ff74b5f1e009e889e5e6
Pedro Henrique <ph6704938@gmail.com>
2026-06-05 09:26:24 -0300

    fix(UserRepository): update email queries for embedded value object

--- src/main/java/com/pedrohenrique/pagcontrolback/repositories/UserRepository.java
+++ src/main/java/com/pedrohenrique/pagcontrolback/repositories/UserRepository.java
@@ -2,13 +2,16 @@ package com.pedrohenrique.pagcontrolback.repositories;
 
 import com.pedrohenrique.pagcontrolback.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
 
+    @Query("SELECT CASE WHEN COUNT(u) > 0 THEN true ELSE false END FROM User u WHERE u.email.value = LOWER(:email)")
     boolean existsUserByEmail(String email);
 
+    @Query("SELECT u FROM User u WHERE u.email.value = LOWER(:email)")
     Optional<User> findByEmail(String email);
 }
