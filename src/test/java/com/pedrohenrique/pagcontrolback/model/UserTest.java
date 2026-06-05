cade630d0a3ef3bce285c57c824369f5f1e966db
Pedro Henrique <ph6704938@gmail.com>
2026-06-05 09:33:02 -0300

    test(UserTest): enhance user constructor tests and add password update validation

--- src/test/java/com/pedrohenrique/pagcontrolback/model/UserTest.java
+++ src/test/java/com/pedrohenrique/pagcontrolback/model/UserTest.java
@@ -1,85 +1,191 @@
 package com.pedrohenrique.pagcontrolback.model;
 
+import com.pedrohenrique.pagcontrolback.exceptions.UserDomainException;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserTest {
 
-    @Test
-    void whenPersonTypesIsPJAndFieldsCorrect_shouldCreateUserSuccessfully() {
-        assertDoesNotThrow(
-                () -> new User(
-                        "John Doe",
-                        "JD Supplies",
-                        "test@gmail.com",
-                        "12345678",
-                        "111-222-3333",
-                        PersonType.PJ
-                )
-        );
+    @Nested
+    class ConstructorTests {
+
+        @Test
+        void shouldCreatePfUserSuccessfully() {
+            User user = new User(
+                    "John Doe",
+                    null,
+                    "test@gmail.com",
+                    "12345678",
+                    "11999999999",
+                    PersonType.PF
+            );
+
+            assertEquals("John Doe", user.getName());
+            assertEquals(PersonType.PF, user.getPersonType());
+            assertNull(user.getFantasyName());
+            assertEquals("test@gmail.com", user.getEmail().value());
+            assertEquals("11999999999", user.getPhone().value());
+            assertNotNull(user.getCreatedAt());
+        }
+
+        @Test
+        void shouldCreatePjUserSuccessfully() {
+            User user = new User(
+                    "Empresa LTDA",
+                    "Minha Empresa",
+                    "empresa@gmail.com",
+                    "12345678",
+                    "11999999999",
+                    PersonType.PJ
+            );
+
+            assertEquals("Empresa LTDA", user.getName());
+            assertEquals(PersonType.PJ, user.getPersonType());
+            assertEquals("Minha Empresa", user.getFantasyName());
+            assertEquals("empresa@gmail.com", user.getEmail().value());
+            assertEquals("11999999999", user.getPhone().value());
+        }
+
+        @Test
+        void shouldThrowWhenNameIsNull() {
+            assertThrows(
+                    UserDomainException.class,
+                    () -> new User(
+                            null,
+                            null,
+                            "test@gmail.com",
+                            "12345678",
+                            "11999999999",
+                            PersonType.PF
+                    )
+            );
+        }
+
+        @Test
+        void shouldThrowWhenNameIsBlank() {
+            assertThrows(
+                    UserDomainException.class,
+                    () -> new User(
+                            "   ",
+                            null,
+                            "test@gmail.com",
+                            "12345678",
+                            "11999999999",
+                            PersonType.PF
+                    )
+            );
+        }
+
+        @Test
+        void shouldThrowWhenPasswordIsNull() {
+            assertThrows(
+                    UserDomainException.class,
+                    () -> new User(
+                            "John Doe",
+                            null,
+                            "test@gmail.com",
+                            null,
+                            "11999999999",
+                            PersonType.PF
+                    )
+            );
+        }
+
+        @Test
+        void shouldThrowWhenPasswordIsBlank() {
+            assertThrows(
+                    UserDomainException.class,
+                    () -> new User(
+                            "John Doe",
+                            null,
+                            "test@gmail.com",
+                            "   ",
+                            "11999999999",
+                            PersonType.PF
+                    )
+            );
+        }
+
+        @Test
+        void shouldThrowWhenPersonTypeIsNull() {
+            assertThrows(
+                    UserDomainException.class,
+                    () -> new User(
+                            "John Doe",
+                            null,
+                            "test@gmail.com",
+                            "12345678",
+                            "11999999999",
+                            null
+                    )
+            );
+        }
+
+        @Test
+        void shouldThrowWhenPfHasFantasyName() {
+            assertThrows(
+                    UserDomainException.class,
+                    () -> new User(
+                            "John Doe",
+                            "Minha Empresa",
+                            "test@gmail.com",
+                            "12345678",
+                            "11999999999",
+                            PersonType.PF
+                    )
+            );
+        }
+
+        @Test
+        void shouldThrowWhenPjHasNoFantasyName() {
+            assertThrows(
+                    UserDomainException.class,
+                    () -> new User(
+                            "Empresa LTDA",
+                            null,
+                            "empresa@gmail.com",
+                            "12345678",
+                            "11999999999",
+                            PersonType.PJ
+                    )
+            );
+        }
+
+        @Test
+        void shouldThrowWhenPjHasBlankFantasyName() {
+            assertThrows(
+                    UserDomainException.class,
+                    () -> new User(
+                            "Empresa LTDA",
+                            "   ",
+                            "empresa@gmail.com",
+                            "12345678",
+                            "11999999999",
+                            PersonType.PJ
+                    )
+            );
+        }
     }
 
-    @Test
-    void whenPersonTypesIsPFAndFieldsCorrect_shouldCreateUserSuccessfully() {
-        assertDoesNotThrow(
-                () -> new User(
-                        "John Doe",
-                        null,
-                        "test@gmail.com",
-                        "12345678",
-                        "111-222-3333",
-                        PersonType.PF
-                )
-        );
+    @Nested
+    class PasswordTests {
+
+        @Test
+        void shouldUpdatePassword() {
+            User user = new User(
+                    "John Doe",
+                    null,
+                    "test@gmail.com",
+                    "12345678",
+                    "11999999999",
+                    PersonType.PF
+            );
+
+            user.setPassword("87654321");
+
+            assertEquals("87654321", user.getPassword());
+        }
     }
-
-
-
-    @Test
-    void whenPersonTypeIsNull_shouldThrowUserDomainException() {
-        assertThrows(
-                com.pedrohenrique.pagcontrolback.exceptions.UserDomainException.class,
-                () -> new User(
-                        "John Doe",
-                        "JD Supplies",
-                        "test@gmail.com",
-                        "12345678",
-                        "111-222-3333",
-                        null
-                )
-        );
-    }
-
-    @Test
-    void whenCreatingUserWithPersonTypeISPFAndNameFantasyNotNull_shouldThrowUserDomainException() {
-        assertThrows(
-                com.pedrohenrique.pagcontrolback.exceptions.UserDomainException.class,
-                () -> new User(
-                        "John Doe",
-                        "JD Supplies",
-                        "test@gmail.com",
-                        "12345678",
-                        "111-222-3333",
-                        PersonType.PF
-                )
-        );
-    }
-
-    @Test
-    void whenCreatingUserWithPersonTypeIsPJAndNoFantasyName_shouldThrowUserDomainException() {
-        assertThrows(
-                com.pedrohenrique.pagcontrolback.exceptions.UserDomainException.class,
-                () -> new User(
-                        "John Doe",
-                        null,
-                        "test@gmail.com",
-                        "12345678",
-                        "111-222-3333",
-                        PersonType.PJ
-                )
-        );
-    }
-
-
 }
