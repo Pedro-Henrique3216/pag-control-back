6f17983059f3eec5b2ce89a9cf033dd7a825bee4
Pedro Henrique <ph6704938@gmail.com>
2026-06-05 09:27:37 -0300

    fix(SupplierRepository): update existsSupplierByCnpjAndUser_Id query for embedded value object

--- src/main/java/com/pedrohenrique/pagcontrolback/repositories/SupplierRepository.java
+++ src/main/java/com/pedrohenrique/pagcontrolback/repositories/SupplierRepository.java
@@ -2,6 +2,7 @@ package com.pedrohenrique.pagcontrolback.repositories;
 
 import com.pedrohenrique.pagcontrolback.model.Supplier;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -9,6 +10,7 @@ import java.util.UUID;
 
 public interface SupplierRepository extends JpaRepository<Supplier, UUID>
 {
+    @Query("SELECT CASE WHEN COUNT(s) > 0 THEN true ELSE false END FROM Supplier s WHERE s.cnpj.value = :cnpj AND s.user.id = :userId")
     boolean existsSupplierByCnpjAndUser_Id(String cnpj, UUID userId);
 
     List<Supplier> findAllByUser_Id(UUID userId);
