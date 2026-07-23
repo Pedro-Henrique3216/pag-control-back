1c53758de7242a6d5f71ccef493bc297ef346b5a
Pedro Henrique <ph6704938@gmail.com>
2026-07-23 15:10:12 -0300

    test(auth): create authenticated users through repository

--- src/test/java/com/pedrohenrique/pagcontrolback/helpers/AuthTestFactory.java
+++ src/test/java/com/pedrohenrique/pagcontrolback/helpers/AuthTestFactory.java
@@ -2,8 +2,12 @@ package com.pedrohenrique.pagcontrolback.helpers;
 
 import com.pedrohenrique.pagcontrolback.dtos.request.UserRequestDto;
 import com.pedrohenrique.pagcontrolback.model.PersonType;
+import com.pedrohenrique.pagcontrolback.model.User;
+import com.pedrohenrique.pagcontrolback.repositories.UserRepository;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
@@ -11,35 +15,30 @@ import java.util.Map;
 @Component
 public class AuthTestFactory {
 
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public void createUser(
             String name,
             String email,
             String password,
-            String phone,
-            int port){
+            String phone
+    ){
 
-        var body = new UserRequestDto(
+        User user = new User(
                 name,
                 null,
                 email,
-                password,
+                passwordEncoder.encode(password),
                 phone,
                 PersonType.PF
         );
 
-        var response = RestAssured
-                .given()
-                .contentType(ContentType.JSON)
-                .body(body)
-                .when()
-                .post("http://localhost:" + port + "/api/users/sign-up")
-                .then()
-                .statusCode(201)
-                .extract()
-                .response()
-                .asString();
+        user.verifyEmail();
 
-        System.out.println(response);
+        userRepository.save(user);
     }
 
     public String loginAndGetToken(int port, String email, String password) {
